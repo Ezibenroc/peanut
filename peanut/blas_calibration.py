10e8453242450714140526fde0f7aec684c6daba
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2019-06-12 16:41:12 +0200

    Add some install options to the BLAS calibration

--- peanut/blas_calibration.py
+++ peanut/blas_calibration.py
@@ -9,6 +9,7 @@ class BLASCalibration(Job):
     all_op = ['dgemm', 'dtrsm']
     expfile_header_in_file = False
     expfile_header = ['operation', 'm', 'n', 'k']
+    installfile_types = {'warmup_time': int, 'multicore': bool, 'openblas': str}
 
     @classmethod
     def check_exp(cls, exp):
@@ -19,6 +20,8 @@ class BLASCalibration(Job):
 
     def setup(self):
         super().setup()
+        assert self.installfile is not None
+        install_options = self.installfile.content
         self.apt_install(
             'build-essential',
             'python3',
@@ -34,8 +37,10 @@ class BLASCalibration(Job):
             'linux-cpupower',
             'numactl',
             'tmux',
+            'stress',
         )
-        self.git_clone('https://github.com/xianyi/OpenBLAS.git', 'openblas', checkout='v0.3.1')
+        openblas_version = install_options['openblas']
+        self.git_clone('https://github.com/xianyi/OpenBLAS.git', 'openblas', checkout=openblas_version)
         self.nodes.run('make -j 64', directory='openblas')
         self.nodes.run('make install PREFIX=%s' % self.nodes.working_dir, directory='openblas')
         self.nodes.run('ln -s libopenblas.so libblas.so', directory='lib')
@@ -47,34 +52,48 @@ class BLASCalibration(Job):
         return self
 
     def run_exp(self):
+        assert self.installfile is not None
+        install_options = self.installfile.content
         assert len(self.expfile) == 1
         expfile = self.expfile[0]
+        warmup = install_options['warmup_time']
+        if warmup > 0:
+            cmd = 'stress -c %d -t %ds' % (4*len(self.nodes.cores), warmup)
+            self.nodes.run(cmd)
         ldlib = 'LD_LIBRARY_PATH=%s/lib' % self.nodes.working_dir
         cmd = './calibrate_blas -s ./zoo_sizes'
         nb_cores = len(self.nodes.cores)
         path = '/tmp/platform-calibration/src/calibration'
         self.nodes.write_files(expfile.raw_content, path + '/zoo_sizes')
-        self.nodes.run('OMP_NUM_THREADS=%d %s %s -o ./result_multicore.csv' % (nb_cores, ldlib, cmd),
-                       directory=path)
-        numactl_str = 'numactl --physcpubind=%d --localalloc'
-        monocore_files = []
-        for i in range(nb_cores):
-            numactl = numactl_str % i
-            filename = 'result_monocore_%d.csv' % i
-            monocore_files.append(filename)
-            command = 'tmux new-session -d -s tmux_%d "OMP_NUM_THREADS=1' % i
-            command += ' %s %s %s -l 2 -o %s"' % (ldlib, numactl, cmd, filename)
-            self.nodes.run(command, directory=path)
-        # Waiting for all the commands to be finished
-        while True:
-            try:
-                time.sleep(60)
-                self.nodes.run('tmux ls')
-            except RunError:
-                break
-        self.add_local_to_archive(path + '/result_multicore.csv')
-        for filename in monocore_files:
-            self.add_local_to_archive(path + '/' + filename)
+        if install_options['multicore']:
+            self.nodes.run('OMP_NUM_THREADS=%d %s %s -o ./result.csv' % (nb_cores, ldlib, cmd),
+                           directory=path)
+            # Adding a column "all" at the end of the CSV, representing the cores used
+            self.nodes.run('awk \'{print $0, "all"}\' %s > tmp && mv tmp %s' % (filename, filename), directory=path)
+        else:
+            numactl_str = 'numactl --physcpubind=%d --localalloc'
+            monocore_files = []
+            for i in range(nb_cores):
+                numactl = numactl_str % i
+                filename = 'result_monocore_%d.csv' % i
+                monocore_files.append(filename)
+                command = 'tmux new-session -d -s tmux_%d "OMP_NUM_THREADS=1' % i
+                command += ' %s %s %s -l 2 -o %s"' % (ldlib, numactl, cmd, filename)
+                self.nodes.run(command, directory=path)
+            # Waiting for all the commands to be finished
+            while True:
+                try:
+                    time.sleep(60)
+                    self.nodes.run('tmux ls')
+                except RunError:
+                    break
+            # Adding a core ID column to each file, then merge all the files into a single one
+            for core, filename in enumerate(monocore_files):
+                self.nodes.run('awk \'{print $0",%d"}\' %s > tmp && mv tmp %s' % (core, filename, filename), directory=path)
+            self.nodes.run('cat %s > ./result.csv' % (' '.join(monocore_files)), directory=path)
+        # Adding a header to the file
+        self.nodes.run("sed -i '1s/^/function,m,n,k,timestamp,duration,core\\n/' ./result.csv", directory=path)
+        self.add_local_to_archive(path + '/result.csv')
 
     @classmethod
     def gen_exp(cls):
