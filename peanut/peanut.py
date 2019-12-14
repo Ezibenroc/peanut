33fb95974b42e2425871b5ab857feae670fbca12
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2019-12-15 00:29:19 +0100

    Update ratatouille

--- peanut/peanut.py
+++ peanut/peanut.py
@@ -765,7 +765,7 @@ class Job:
 
     def start_monitoring(self, period=1):
         self.git_clone('https://github.com/Ezibenroc/ratatouille.git', 'ratatouille',
-                        checkout='05373918e071e563478c9262f90d98b24e1e9165')
+                        checkout='323ed812d816c332a117b4132a6a8ba459fbad05')
         self.nodes.run('pip3 install .', directory='ratatouille')
         self.nodes.run('ratatouille --git-version')
         command = 'ratatouille collect -t %d monitoring.csv' % period
