41f63c3329b904f657725ffc16318eac0a031e44
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-06-08 15:34:34 +0200

    Update ratatouille (power monitoring)

--- peanut/peanut.py
+++ peanut/peanut.py
@@ -765,7 +765,7 @@ class Job:
 
     def start_monitoring(self, period=1):
         self.git_clone('https://github.com/Ezibenroc/ratatouille.git', 'ratatouille',
-                        checkout='bb1ac5e61428a2390755c4878c056040e04f8b77')
+                        checkout='1f1282e6d26f37f489b3e94f687485196031ed81')
         self.nodes.run('pip3 install .', directory='ratatouille')
         self.nodes.run('ratatouille --git-version')
         command = 'ratatouille collect -t %d all monitoring.csv' % period
