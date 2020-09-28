e7fc2371be2130c2a128cd380c741727dc6ace80
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-09-28 15:02:38 +0200

    Add the output of dmesg

--- peanut/peanut.py
+++ peanut/peanut.py
@@ -864,6 +864,7 @@ class Job:
                     'lspci.txt': 'lspci -v > lspci.txt',
                     'dmidecode.txt': 'dmidecode > dmidecode.txt',
                     'lsmod.txt': 'lsmod > lsmod.txt',
+                    'dmesg.txt': 'dmesg > dmesg.txt',
                     }
         for filename, command in commands_with_files.items():
             self.__add_raw_information_to_archive(filename, command)
